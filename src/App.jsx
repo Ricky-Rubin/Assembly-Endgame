d51ce6c8a59ad339c0d79d6cbdec6aecd715f332
Owolabi <owolabi1738@gmail.com>
2025-09-22 13:28:16 +0100

    Added a sectionn element and styled it

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,11 @@ export default function App() {
   return (
     <main>
       <Header />
+
+      <section className="result">
+        <h2>You Win!</h2>
+        <p>Well done 🎉</p>
+      </section>
     </main>
   )
 }
