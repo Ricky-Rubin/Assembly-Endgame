cb0fbc65216861f2d03d5f4a9d8109ac198fd267
Owolabi <owolabi1738@gmail.com>
2025-09-22 12:51:34 +0100

    Styling changes

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,7 @@
 export default function Header() {
     return (
         <header>
-            <h3>Assembly: Endgame</h3>
+            <h2>Assembly: Endgame</h2>
             <p>Guess the word in under 8 attempts to keep the programming world safe from Assembly!</p>
         </header>
     )
