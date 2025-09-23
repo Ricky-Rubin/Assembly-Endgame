1347ae81aa1a6ed695c19b5a62efa0d02c57994b
Owolabi <owolabi1738@gmail.com>
2025-09-23 11:35:49 +0100

    Updated section style. Created an array of objects, rendered them via a mapped variable in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
 import './App.css'
 import Header from "./Header.jsx"
+import { Language } from "./Language.js"
 
 export default function App() {
+  const mappedChips = Language.map((chip, index) => {
+    return <div key={index} className="chip-case">{chip.name}</div>
+  })
 
   return (
     <main>
@@ -11,6 +15,10 @@ export default function App() {
         <h2>You Win!</h2>
         <p>Well done 🎉</p>
       </section>
+
+      <div className="chip-container">
+        {mappedChips}
+      </div>
     </main>
   )
 }
