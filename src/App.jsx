a7c4b808665db1140c03c11de43ec9793ab676ea
Owolabi <owolabi1738@gmail.com>
2025-09-23 13:32:56 +0100

    Added and styled elements to display word puzzle

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
 import './App.css'
+import React from "react"
 import Header from "./Header.jsx"
 import { Language } from "./Language.js"
 
 export default function App() {
+
+  const [currentWord, setCurrentWord] = React.useState('react');
+
+  const wordSplit = currentWord.split('');
+  const mappedSplit = wordSplit.map((letter) => {
+    return <span className="split-word-letters">{letter.toUpperCase()}</span>
+  })
+
   const mappedChips = Language.map((chip, index) => {
     return <div style={{ backgroundColor: chip.backgroundColor, color: chip.color }} key={index} className="chip-case">{chip.name}</div>
   })
@@ -18,6 +27,10 @@ export default function App() {
 
       <div className="chip-container">
         <div className="sub-container">{mappedChips}</div>
+      </div>
+
+      <div className="word-display">
+        {mappedSplit}
       </div>
     </main>
   )
